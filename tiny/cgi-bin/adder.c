0a9e4df2c72fc9c1bf61409ee2fce526a069d41e
brido4125 <hcs4125@gmail.com>
2022-11-07 10:33:51 +0900

    update:HEAD 요청이 다이나믹

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,6 +4,7 @@ int main(void)
 {
     char *buf, *p;
     char *method;
+    method = getenv("REQUEST_METHOD");
     char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
     int n1 = 0, n2 = 0;
     /* Extract the two arguments */
@@ -16,7 +17,6 @@ int main(void)
         n1 = atoi(arg1);
         n2 = atoi(arg2);
     }
-    method = getenv("REQUEST_METHOD");
     /* Make the response body */
     sprintf(content, "QUERY_STRING=%s", buf);
     sprintf(content, "Welcome to add.com: ");
@@ -24,23 +24,17 @@ int main(void)
     sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
             content, n1, n2, n1 + n2);
     sprintf(content, "%sThanks for visiting! \r\n", content);
-
-    if (strcasecmp(method, "HEAD") == 0) {
-        sprintf(content, "%sConnection: close\r\n", content);
-        sprintf(content, "%sContent-length: %lu\r\n", content, strlen(content));
-        sprintf(content, "%sContent-type: text/html\r\n\r\n", content);
-        printf("Connection : close\r\n");
-        printf("Content-length: %d\r\n", (int)strlen(content));
-        printf("Content-type : text/html \r\n\r\n");
-
-        fflush(stdout);
-
-        exit(0);
-    }
     /* Generate the HTTP response */
     printf("Connection : close\r\n");
     printf("Content-length: %d\r\n", (int)strlen(content));
     printf("Content-type : text/html \r\n\r\n");
+
+    if (!strcasecmp(method, "HEAD"))
+    {
+        fflush(stdout);
+        exit(0);
+    }
+
     printf("%s", content);
     fflush(stdout);
     exit(0);
